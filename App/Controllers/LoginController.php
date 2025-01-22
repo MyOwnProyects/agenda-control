862b62f282f71a894fd707c5f866ddb22b7865ea
Hugo Munoz <proyectos.munoz17@gmail.com>
2025-01-22 10:47:20 -0700

    Curl interno funcionando

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -30,7 +30,39 @@ class LoginController extends Controller
 
             // Lógica de autenticación (aquí solo es un ejemplo)
             if ($username === 'admin' && $password === '1234') {
-                // Si la autenticación es exitosa, se puede redirigir o devolver un mensaje
+
+                // URL de la API, usando el dominio configurado en Nginx
+                $apiUrl = 'http://nginx/ctusuarios/show';
+
+                // Configurar cURL
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $apiUrl);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                    'Content-Type: application/json',
+                ]);
+                // Especificar explícitamente el método GET (opcional, ya que es el predeterminado)
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+                //curl_setopt($ch, CURLOPT_RESOLVE, ['agenda-control-api.local:80:127.0.0.1']);
+
+
+                // Ejecutar la solicitud
+                $response = curl_exec($ch);
+
+                // Manejar errores
+                if (curl_errno($ch)) {
+                    $error = curl_error($ch);
+                    curl_close($ch);
+                    return $this->response->setJsonContent(['error' => $error]);
+                }
+
+                curl_close($ch);
+
+                // Devolver la respuesta como JSON
+                return $this->response->setJsonContent(json_decode($response, true));
+
+
+                /* // Si la autenticación es exitosa, se puede redirigir o devolver un mensaje
                 $response = new Response();
                 $response->setStatusCode(200, "OK");
                 $response->setJsonContent([
@@ -38,7 +70,7 @@ class LoginController extends Controller
                     'message' => 'Inicio de sesión exitoso.',
                     'redirect' => '/dashboard'  // Aquí puedes enviar la URL a la que se redirige al usuario
                 ]);
-                return $response;
+                return $response;*/
             } else {
                 // Si las credenciales son incorrectas
                 $response = new Response();
