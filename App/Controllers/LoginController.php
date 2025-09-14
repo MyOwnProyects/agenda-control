ef9c920ce366250049c6b5786f63dd9adac71eef
Hugo Munoz <proyectos.munoz17@gmail.com>
2025-09-14 16:14:59 -0700

    1. Ojo de pasword en login, asi como obtencion de bfp. 2. se guarda en sesion el navegador que se esta utilizando. 3. Bitacora de acceso al sistema. 4. Nombre de usuario visible

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -12,12 +12,14 @@ class LoginController extends Controller
 {
     protected $rutas;
     protected $url_api;
+    protected $bitacora;
 
     public function initialize(){
         $config         = $this->getDI();
         $this->rutas    = $config->get('rutas');
         $config         = $config->get('config');
         $this->url_api  = $config['BASEAPI'];
+        $this->bitacora = 'Login';
     }
     
     public function indexAction()
@@ -28,8 +30,9 @@ class LoginController extends Controller
 
         if ($this->request->isAjax() && $this->request->isPost()){
             // Obtener los datos del formulario
-            $username = $this->request->getPost('username', 'string');
-            $password = $this->request->getPost('password', 'string');
+            $username   = $this->request->getPost('username', 'string');
+            $password   = $this->request->getPost('password', 'string');
+            $bfp        = $this->request->getPost('bfp', 'string');
 
             // Validación de los datos (ejemplo simple)
             if (empty($username) || empty($password)) {
@@ -70,15 +73,18 @@ class LoginController extends Controller
                     return $response;
                 }
 
+                $tmp_navegador  = $this->get_browser_info($_SERVER['HTTP_USER_AGENT']);
+
                 $this->session->set("clave",$request[0]['clave']);
-                $this->session->set("primer_apellido",$request[0]['clave']);
-                $this->session->set("segundo_apellido",$request[0]['clave']);
-                $this->session->set("nombre",$request[0]['clave']);
-                $this->session->set("clave_tipo_usuario",$request[0]['clave']);
-                $this->session->set("nombre_tipo_usuario",$request[0]['clave']);
+                $this->session->set("primer_apellido",$request[0]['primer_apellido']);
+                $this->session->set("segundo_apellido",$request[0]['segundo_apellido']);
+                $this->session->set("nombre",$request[0]['nombre']);
+                $this->session->set("clave_tipo_usuario",$request[0]['clave_tipo_usuario']);
+                $this->session->set("nombre_tipo_usuario",$request[0]['nombre_tipo_usuario']);
                 $this->session->set("permisos",array());
                 $this->session->set("language",'es');
-                
+                $this->session->set("bfp",$bfp);
+                $this->session->set("navegador",$this->get_browser_info($_SERVER['HTTP_USER_AGENT']));
 
                 //  CREACION DE SESION
                 $route      = $this->url_api.$this->rutas['ctusuarios']['get_info_usuario'];
@@ -88,6 +94,8 @@ class LoginController extends Controller
                 $request    = FuncionesGlobales::RequestApi('GET',$route,$params);
 
                 $this->session->set("permisos",$request['permisos']);
+
+                FuncionesGlobales::saveBitacora($this->bitacora,'ACCESO','Acceso a plataforma',array());
 
                 $response->setStatusCode(200, "OK");
                 $response->setJsonContent([
@@ -101,10 +109,24 @@ class LoginController extends Controller
         }
     }
 
-    public function logoutAction()
-    {
+    public function logoutAction(){
         // Destruir la sesión y redirigir a login
         $this->session->destroy();
         $this->response->redirect('/');
     }
+
+    function get_browser_info($user_agent){
+        $browser = 'Other';
+        if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) $browser = 'Opera';
+        elseif (strpos($user_agent, 'Edge')) $browser = 'Edge';
+        elseif (strpos($user_agent, 'Chrome')) $browser = 'Chrome';
+        elseif (strpos($user_agent, 'Safari')) $browser = 'Safari';
+        elseif (strpos($user_agent, 'Firefox')) $browser = 'Firefox';
+        elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) $browser = 'Internet Explorer';
+
+        $is_mobile = preg_match('/android|iphone|ipad|ipod|blackberry|windows phone|opera mini|mobile/i', strtolower($user_agent));
+
+        return $browser . ($is_mobile ? ' Mobile' : '');
+    }
+
 }
