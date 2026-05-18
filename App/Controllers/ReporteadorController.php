360e4506c7f79b06b4f7e6052d2d718fb720001f
Hugo Munoz <proyectos.munoz17@gmail.com>
2026-05-17 19:38:51 -0700

    Modificacion a los filtros de busqueda para incluir movimientos futuros a las fechas de busqueda

--- App/Controllers/ReporteadorController.php
+++ App/Controllers/ReporteadorController.php
@@ -687,6 +687,104 @@ class ReporteadorController extends BaseController
                 ],
             ]);
 
+            //  INFORMACION A MOSTRAR
+            $sheet3->setCellValue('A9', 'TIPO');
+            $sheet3->setCellValue('B9', 'PACIENTE');
+            $sheet3->setCellValue('C9', 'FECHA');
+            $sheet3->setCellValue('D9', 'MONTO');
+            $sheet3->setCellValue('E9', 'ESTATUS');
+            $sheet3->setCellValue('F9', 'MÉTODO DE PAGO');
+            $sheet3->setCellValue('G9', 'MONTO DISPONIBLE');
+
+            $sheet3->getStyle('A9:G9')->applyFromArray([
+                'font' => [
+                    'bold'  => true,
+                    'color' => ['argb' => 'FFFFFFFF'],
+                ],
+                'fill' => [
+                    'fillType'   => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'startColor' => ['argb' => 'FF000000'],
+                ],
+            ]);
+
+            $columna    = 10;
+            foreach ($arr_rows['hoja_3'] as $row) {
+                $sheet3->setCellValue('A'.$columna, 'ABONO');
+                $sheet3->setCellValue('B'.$columna, $row['nombre_completo']);
+                $sheet3->setCellValue('C'.$columna, $row['fecha_hora_pago']);
+                $sheet3->setCellValue('D'.$columna, '$'.FuncionesGlobales::formatoMonetario($row['monto']));
+                $sheet3->setCellValue('E'.$columna, $row['label_estatus_abono']);
+                $sheet3->setCellValue('F'.$columna, $row['metodo_pago']);
+                $sheet3->setCellValue('G'.$columna, '$'.FuncionesGlobales::formatoMonetario($row['monto_disponible']));
+
+                $sheet3->getStyle('A'.$columna.':G'.$columna)->applyFromArray([
+                    'font' => [
+                        'bold'  => true,
+                        'color' => ['argb' => 'FF000000'],
+                    ],
+                    'fill' => [
+                        'fillType'   => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                        'startColor' => ['argb' => 'FFD9E1F2'],
+                    ],
+                ]);
+
+                $columna++;
+                //  MUESTRA LA INFORMACION DE LOS MOVIMIENTOS
+                if (count($row['info_movtos']) > 0){
+                    foreach($row['info_movtos'] as $row_movto){
+                        
+
+                        $sheet3->setCellValue('A'.$columna, 'MOVIMIENTO');
+                        $sheet3->setCellValue('B'.$columna, '-');
+                        $sheet3->setCellValue('C'.$columna, $row_movto['fecha_hora_pago']);
+                        $sheet3->setCellValue('D'.$columna, '$'.FuncionesGlobales::formatoMonetario($row_movto['monto']));
+                        $sheet3->setCellValue('E'.$columna, $row_movto['label_estatus_movto']);
+                        $sheet3->setCellValue('F'.$columna, '-');
+                        $sheet3->setCellValue('G'.$columna, '-');
+
+                        $sheet3->getStyle('A'.$columna)->applyFromArray([
+                            'font' => [
+                                'italic' => true,
+                                'color'  => ['argb' => 'FF666666'],
+                            ],
+                            'alignment' => [
+                                'indent'     => 2,
+                                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT,
+                            ],
+                        ]);
+
+                        $columna++;
+                    }
+                } else {
+                    
+
+                    $sheet3->setCellValue('A'.$columna, 'SIN MOVIMIENTOS');
+                    $sheet3->setCellValue('B'.$columna, '-');
+                    $sheet3->setCellValue('C'.$columna, '-');
+                    $sheet3->setCellValue('D'.$columna, '-');
+                    $sheet3->setCellValue('E'.$columna, '-');
+                    $sheet3->setCellValue('F'.$columna, '-');
+                    $sheet3->setCellValue('G'.$columna, '-');
+
+                    $sheet3->getStyle('A'.$columna)->applyFromArray([
+                        'font' => [
+                            'italic' => true,
+                            'color'  => ['argb' => 'FF666666'],
+                        ],
+                        'alignment' => [
+                            'indent'     => 2,
+                            'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT,
+                        ],
+                    ]);
+
+                    $columna++;
+                }
+            }
+
+            foreach (range('A', 'G') as $col) {
+                $sheet3->getColumnDimension($col)->setAutoSize(true);
+            }
+
             $spreadsheet->setActiveSheetIndex(0);
 
             $path_file = FuncionesGlobales::get_path_file('reportes');
