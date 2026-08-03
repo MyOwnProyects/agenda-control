d5db49e1a5b69a7801fe8cdc284e0580486949ae
Hugo Munoz <proyectos.munoz17@gmail.com>
2026-08-02 20:54:19 -0700

    Opcion de transferir horario fijo de un profesional a otro

--- public/language/es.php
+++ public/language/es.php
@@ -204,6 +204,9 @@ return [
     'html_title_reporter'                       => 'Reporteador',
     'html_message_templates'                    => 'Plantillas de mensajes',
     'html_movil_agenda'                         => 'Agenda movil',
+    'html_view_fixed_schedule'                  => 'Ver horario fijo',
+    'html_fixed_schedule'                       => 'Horario fijo',
+    'html_transfer_appointment'                 => 'Transferir cita',
     
 
 
